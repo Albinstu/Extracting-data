59b300f0ff23ae2a09847d88db8e81a0da3c85ab
Stu <alb.stu@gmail.com>
2022-05-05 22:34:48 +0200

    Update OU2_matstat.py

--- OU2_matstat.py
+++ OU2_matstat.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  2 15:12:59 2022
-
-@author: stu
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 import re
-# import pandas
-# import math
 
-file_path = '/Users/stu/Library/CloudStorage/OneDrive-Privat/UU/Matematisk Statestik/OU2/IceCube40StringData.txt'
+file_path = '[you file path]/IceCube40StringData.txt'
 
 def load_file(file_path):
     with open(file_path, 'r') as file:
@@ -25,21 +15,17 @@ def load_file(file_path):
                 header = re.findall(r'[a-zA-z/]+', next(file, None))
                 print(header)
                 break
-            
-        # cunt = np.array(list(map(float, re.findall(r'[0-9.]*[0-9]+', next(file, None)))))
+
         next(file, None)
         pp = [list(map(float, re.findall(r'[0-9.]*[0-9]+', line))) for line in file]
-        # print(pp)
-        print(pp.pop(-1)) #tar bort det sista värdet då det ej passar in, är dessutom antal sekunder på ett dygn
-        # for line in file:
-            # np.append(cunt, list(map(float, re.findall(r'[0-9.]*[0-9]+', line))))
+
+        pp.pop(-1) #tar bort det sista värdet då det ej passar in, är dessutom antal sekunder på ett dygn
         return np.array(pp)
 
 
 data = load_file(file_path)
 # print(data)
 
-# skrivit kod för data extrahering
 
 
         
